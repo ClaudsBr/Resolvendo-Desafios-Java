21b8269edb2fa4c8abbf21449ab72715e8ca4542
ClaudsBr <clauds.true@gmail.com>
2021-07-30 12:42:33 -0300

    Uploading the second challenge

--- src/desafiosemjava/IdadeEmDias.java
+++ src/desafiosemjava/IdadeEmDias.java
@@ -1,28 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package desafiosemjava;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Claudio
- */
+
 public class IdadeEmDias {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
+        
+        /*
+        
+        Leia um valor inteiro correspondente à idade de uma pessoa em dias e 
+        informe-a em anos, meses e dias.
+        Obs.: apenas para facilitar o cálculo, considere todo ano com 365 dias 
+        e todo mês com 30 dias. Nos casos de teste nunca haverá uma situação 
+        que permite 12 meses e alguns dias, como 360, 363 ou 364. Este é apenas 
+        um exercício com objetivo de testar raciocínio matemático simples.
+        
+        Entrada
+        O arquivo de entrada contém um valor inteiro.
+        
+        Saída
+        Imprima a saída conforme exemplo fornecido.
+        
+        */
+        
+        //setup
         Scanner entrada = new Scanner(System.in);
-        int dias = entrada.nextInt();        
+        int dias;      
         int mes = 0;
         int ano = 0;
         
+        // entrada
+        dias = entrada.nextInt();
         
+        //preocessamento
         do{
             dias -=365;
             ano++;
@@ -35,7 +47,13 @@ public class IdadeEmDias {
             
         }while(dias >=30);
         
-        System.out.println("Ano(s) = "+ano+"\nMes(es) = "+mes+ "\nDia(s) = "+dias);
+        //saida
+        System.out.println(ano+" ano(s)");
+        System.out.println(mes+" mes(es)");
+        System.out.println(dias+" dia(s)");
+        
+        //fechamento
+        entrada.close();
     }
     
 }
